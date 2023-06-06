de9e3b4e0d8deb742de8db10e08d5612f4782830
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2023-06-07 01:51:30 +0530

    chnages layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,12 +3,18 @@ export const Footer = () => {
   return (
     <footer className="mainFooter">
       <div className="row primary">
-        <div
-          style={{ display: "flex", flexDirection: "column" }}
-          className="column about"
-        >
-          <h3>Djikstra E-Commerce</h3>
-          <p>Lorem ipsum dolor sit</p>
+        <div className="column about">
+          <h3>DjikstraExpress</h3>
+          <p>
+            Unlock the world of endless possibilities with Djiktra, your
+            ultimate destination for hassle-free online shopping in India.
+            <br />
+            Experience the convenience of Djiktra, where every click connects
+            you to a world of incredible products and unbeatable deals
+            <br />
+            Djiktra - your one-stop shop for all your shopping needs, where
+            convenience, affordability, and reliability come together
+          </p>
         </div>
         <div className="column link">
           <h3>Links</h3>
@@ -47,16 +53,16 @@ export const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="column subscribe">
+        {/* <div className="column subscribe">
           <h3>Newsletter</h3>
           <div>
             <input type="email" placeholder="parth.kathuria99@gmail.com" />
             <button disabled>Subscribe</button>
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="row copyright">
-        <p>Copyright Parth Kathuria &copy; 2023</p>
+        <p>Parth Kathuria &copy; 2023</p>
       </div>
     </footer>
   );
